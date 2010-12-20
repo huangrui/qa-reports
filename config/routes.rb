9247a79d4cd343a85952f030744f63d7cbd22a50
Janne Hietamaki <janne.hietamaki@leonidasoy.fi>
2010-12-20 16:49:10 +0200

    Fixing route ordering a bit more

--- config/routes.rb
+++ config/routes.rb
@@ -27,14 +27,14 @@ Meegoqa::Application.routes.draw do
     
     match '/:release_version/:target/:testtype/compare/:comparetype' => 'reports#compare', :via => "get"
 
+    match '/:release_version/:target/:testtype/:hwproduct/paging/:page' => 'index#filtered_list', :via => "get"
+    match '/:release_version/:target/:testtype/paging/:page' => 'index#filtered_list', :via => "get"
+    match '/:release_version/:target/paging/:page' => 'index#filtered_list', :via => "get"
+
     match '/:release_version/:target/:testtype/:hwproduct/:id' => 'reports#view', :via => "get"
     match '/:release_version/:target/:testtype/:hwproduct/:id/edit' => 'reports#edit', :via => "get"
     match '/:release_version/:target/:testtype/:hwproduct/:id/delete' => 'reports#delete', :via => "post"
     match '/:release_version/:target/:testtype/:hwproduct/:id/print' => 'reports#print', :via => "get"
-
-    match '/:release_version/:target/:testtype/:hwproduct/paging/:page' => 'index#filtered_list', :via => "get"
-    match '/:release_version/:target/:testtype/paging/:page' => 'index#filtered_list', :via => "get"
-    match '/:release_version/:target/paging/:page' => 'index#filtered_list', :via => "get"
 
     match '/:release_version/:target/:testtype/:hwproduct' => 'index#filtered_list', :via => "get"
     match '/:release_version/:target/:testtype' => 'index#filtered_list', :via => "get"
