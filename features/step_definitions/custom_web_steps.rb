57334e10512e99c17c54abcfe0c03a1e90d88262
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-01-11 08:40:54 +0200

    refactored cucumber support files to allow regeneration without losing custom changes

--- features/step_definitions/custom_web_steps.rb
+++ features/step_definitions/custom_web_steps.rb
@@ -6,3 +6,19 @@ When /submit the form(?: at "([^"]*)")?$/ do |form_id|
   target = form_id || "input[@type='submit']"
   click(target)
 end
+
+
+Then /^the link "([^"]*)" within "([^"]*)" should point to the report "([^"]*)"/ do |link, selector, expected_report|
+  with_scope(selector) do
+    field = find_link(link)
+
+    version, target, test_type, hardware = expected_report.downcase.split('/')
+    report = MeegoTestSession.first(:conditions =>
+     {:release_version => version, :target => target, :hwproduct => hardware, :testtype => test_type}
+    )
+    raise "report not found with parameters #{version}/#{target}/#{hardware}/#{test_type}!" unless report
+
+    field[:href].should == "/#{version.capitalize}/#{target.capitalize}/#{test_type.capitalize}/#{hardware.capitalize}/#{report.id}"
+  end
+end
+
