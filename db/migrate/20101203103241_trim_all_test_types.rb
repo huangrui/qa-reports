900e3e88554a6699735ea90322c5a12c3ba5a294
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-01-21 17:12:37 +0200

    data migrations removed

--- db/migrate/20101203103241_trim_all_test_types.rb
+++ db/migrate/20101203103241_trim_all_test_types.rb
@@ -1,10 +1,10 @@
 class TrimAllTestTypes < ActiveRecord::Migration
   def self.up
-    sessions = MeegoTestSession.find(:all)
-    sessions.each do |s|
-      s.testtype = s.testtype.strip()
-      s.save
-    end
+#    sessions = MeegoTestSession.find(:all)
+#    sessions.each do |s|
+#      s.testtype = s.testtype.strip()
+#      s.save
+#    end
   end
 
   def self.down
