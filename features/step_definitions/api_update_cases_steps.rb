4e752b26e0ee68ebd946946f6e156538a58b9be5
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-01-24 14:51:04 +0200

    removed duplicate step definitions

--- features/step_definitions/api_update_cases_steps.rb
+++ features/step_definitions/api_update_cases_steps.rb
@@ -1,16 +1,6 @@
-Given /^I am an user with a REST authentication token$/ do
-  Given %{I have one user "John Restless" with email "resting@man.net" and password "secretpass" and token "foobar"}
-end
-
-When /^the client sends file "([^\"]*)" via the REST API$/ do |file|
-  post "/api/update/1?auth_token=foobar", {
+When /^the client updates the report with id (\d+) with file "([^\"]*)" via the REST API$/ do |id, file|
+  post "/api/update/#{id}?auth_token=foobar", {
       "report"          => Rack::Test::UploadedFile.new("features/resources/#{file}", "text/xml")
   }
   response.should be_success
-end
-
-Then /^the REST result "([^\"]*)" is "([^\"]*)"$/ do |key, value|
-  json = ActiveSupport::JSON.decode(@response.body)
-  key.split('|').each { |item| json = json[item] }
-  json.should == value
 end
