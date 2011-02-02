1ce9595c8f2b92e10aa456eef403522c01617533
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-02-02 15:01:26 +0200

    fixed seed data handling for javascript test cases

--- features/support/custom_env.rb
+++ features/support/custom_env.rb
@@ -3,4 +3,6 @@ require 'capybara/envjs'
 Capybara.ignore_hidden_elements = true
 Capybara.javascript_driver = :envjs
 
-require "#{Rails.root}/db/seeds.rb"
+Before do
+  load "#{Rails.root}/db/seeds.rb"
+end
