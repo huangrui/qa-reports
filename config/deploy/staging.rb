972ab2545288488552c0f75222d590714054546b
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-02-25 17:01:43 +0200

    fixed couple of typos in deployment script

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -13,8 +13,8 @@ server "staging.leonidasoy.fi", :app, :web, :db, :primary => true
 namespace :db do
   desc "Import production database to staging"
   task :import, :roles => :db, :only => {:primary => true} do
-    put "./qa_reports_production.sql.bz2" "#{current_path}/qa_reports_production.sql.bz2"
+    put "./qa_reports_production.sql.bz2", "#{current_path}/qa_reports_production.sql.bz2"
     run "cd #{current_path} && RAILS_ENV='staging' rake db:import_to_db"
-    run "rm #{current_path}/qa_reports_production.sql.bz2"
+    run "rm #{current_path}/qa_reports_production.sql"
   end
 end
