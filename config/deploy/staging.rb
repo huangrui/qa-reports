be64f2fc914bd3c1684e122bf79d550b88a43808
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-02-25 17:12:11 +0200

    changed 'put' to 'upload'

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -13,7 +13,7 @@ server "staging.leonidasoy.fi", :app, :web, :db, :primary => true
 namespace :db do
   desc "Import production database to staging"
   task :import, :roles => :db, :only => {:primary => true} do
-    put "./qa_reports_production.sql.bz2", "#{current_path}/qa_reports_production.sql.bz2"
+    upload "./qa_reports_production.sql.bz2", "#{current_path}/qa_reports_production.sql.bz2"
     run "cd #{current_path} && RAILS_ENV='staging' rake db:import_to_db"
   end
 end
