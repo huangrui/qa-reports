53d389a4a2f590bc33c3c0c465a7a88d6f2a690c
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-03-08 12:03:15 +0200

    added sort-index to measurement series migration

--- db/migrate/20110304131808_create_nft_series_table.rb
+++ db/migrate/20110304131808_create_nft_series_table.rb
@@ -2,12 +2,14 @@ class CreateNftSeriesTable < ActiveRecord::Migration
   def self.up
     create_table :serial_measurements do |t|
       t.integer :meego_test_case_id, :null => false
-      t.string :short_json, :limit => 256
-      t.text   :long_json,  :limit => 1000
-      t.float :min_value
-      t.float :max_value
-      t.float :avg_value
-      t.float :median_value
+      t.integer :sort_index, :null => false
+      t.string :short_json, :limit => 256, :null => false
+      t.text   :long_json,  :limit => 1000, :null => false
+      t.string :unit, :limit => 32, :null => false
+      t.float :min_value, :null => false
+      t.float :max_value, :null => false
+      t.float :avg_value, :null => false
+      t.float :median_value, :null => false
     end
 
     add_index :serial_measurements, :meego_test_case_id
