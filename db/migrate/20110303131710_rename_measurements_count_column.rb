bb18b18fafd82638bad196fd138a1ae99aaf8dd5
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-03-08 18:07:19 +0200

    removed obsolete migrations

--- db/migrate/20110303131710_rename_measurements_count_column.rb
+++
@@ -1,9 +0,0 @@
-class RenameMeasurementsCountColumn < ActiveRecord::Migration
-  def self.up
-    rename_column :meego_test_cases, :measurements_count, :meego_measurements_count
-  end
-
-  def self.down
-    rename_column :meego_test_cases, :meego_measurements_count, :measurements_count
-  end
-end
