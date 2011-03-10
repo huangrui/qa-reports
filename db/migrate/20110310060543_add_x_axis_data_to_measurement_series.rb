f42a3b01646ee1b02c48f42a7962d020106f1d47
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-03-10 18:46:43 +0200

    added storing interval unit to db

--- db/migrate/20110310060543_add_x_axis_data_to_measurement_series.rb
+++
@@ -1,9 +0,0 @@
-class AddXAxisDataToMeasurementSeries < ActiveRecord::Migration
-  def self.up
-    add_column :serial_measurements, :xaxis_json, :text, :null => false, :default => "" 
-  end
-
-  def self.down
-    remove_column :serial_measurements, :xaxis_json
-  end
-end
