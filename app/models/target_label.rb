0fc334fea758b1b1c6eb1ec6c0fef7b72352aa78
Vesa Poikajärvi <poikajar@poikajar-VirtualBox.(none)>
2011-03-21 16:02:03 +0200

    Fix for not having any target preselected on first upload

--- app/models/target_label.rb
+++ app/models/target_label.rb
@@ -2,4 +2,8 @@ class TargetLabel < ActiveRecord::Base
   def self.labels
     find(:all, :select => "normalized").map(&:normalized)
   end
+
+  def self.first_label
+    first(:conditions => ["sort_order = 0"], :select => "label").label
+  end
 end
