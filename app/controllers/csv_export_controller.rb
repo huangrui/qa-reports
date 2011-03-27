546d8466a2a4beb2eebebb7d075a9c097f6cbacf
Huang Rui <rui.r.huang@intel.com>
2011-03-27 18:45:04 +0800

    Implement the feature FEA#13526: Request to get the detailed test results as CSV file from an existed report
    I have add the buttom (Download as CSV)in report view. After press it,
    the existed report is able to be exported as CSV file, and we can upload
    it as a new report.
    
    Signed-off-by: Huang Rui <rui.r.huang@intel.com>

--- app/controllers/csv_export_controller.rb
+++ app/controllers/csv_export_controller.rb
@@ -1,6 +1,7 @@
 require 'csv_generator'
 
 class CsvExportController < ApplicationController
+
   def export
     csv = CsvGenerator::generate_csv(
             VersionLabel.where(:normalized => @selected_release_version.downcase).first().id,
@@ -10,5 +11,17 @@ class CsvExportController < ApplicationController
     )
 
     send_data csv, :type => "text/plain", :filename=>"entries.csv", :disposition => 'attachment'
+  end
+
+  def export_report
+    csv = CsvGenerator::generate_csv_report(
+            VersionLabel.where(:normalized => @selected_release_version.downcase).first().id,
+            params[:target],
+            params[:testtype],
+            params[:hwproduct],
+            params[:id]
+    )
+
+    send_data csv, :type => "test/plain", :filename=>"report.csv", :disposition => 'attachment'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,7 @@ Meegoqa::Application.routes.draw do
     match '/:release_version/:target/:testtype/:hwproduct/:id/edit' => 'reports#edit', :via => "get"
     match '/:release_version/:target/:testtype/:hwproduct/:id/delete' => 'reports#delete', :via => "post"
     match '/:release_version/:target/:testtype/:hwproduct/:id/print' => 'reports#print', :via => "get"
+    match '/:release_version/:target/:testtype/:hwproduct/:id/download' => 'csv_export#export_report', :via => "get"
 
     match '/:release_version/:target/:testtype/:hwproduct' => 'index#filtered_list', :via => "get"
     match '/:release_version/:target/:testtype' => 'index#filtered_list', :via => "get"
