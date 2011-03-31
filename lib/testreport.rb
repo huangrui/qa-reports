b737d8e99472b56024d8b515ba63b34c29130731
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-03-31 11:26:14 +0300

    removed unneeded calculate_feature_pass_rate method

--- lib/testreport.rb
+++ lib/testreport.rb
@@ -22,23 +22,6 @@
 
 module MeegoTestReport
 
-  def MeegoTestReport.calculate_feature_pass_rate(total, pass)
-    if not total
-        "n/a"
-    elsif not pass
-        "0"
-    else
-        total_int = total.to_i()
-        pass_int = pass.to_i()
-        if total_int <= 0
-           "n/a"
-        else
-           rate = pass_int*100.0/total_int
-           "%i%%" % rate
-        end
-    end
-  end
-  
   def MeegoTestReport.find_bugzilla_ids(txt)
     ids = Set()
     txt.scan /#{BUGZILLA_CONFIG['link_uri']}(\d+)/.each do |match|
