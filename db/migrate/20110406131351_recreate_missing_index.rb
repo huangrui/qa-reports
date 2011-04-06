32b76127324e30f46c302cf756c6a7495d9fd878
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-04-06 16:23:29 +0300

    fix to migration error supression

--- db/migrate/20110406131351_recreate_missing_index.rb
+++ db/migrate/20110406131351_recreate_missing_index.rb
@@ -4,7 +4,7 @@ class RecreateMissingIndex < ActiveRecord::Migration
   def self.up   
     add_index :meego_test_sessions, [:release_version, :target, :testtype, :hwproduct], :name => 'index_meego_test_sessions_key'
   rescue
-    raise unless $!.message =~ /^Mysql::Error: Duplicate key name/i
+    raise unless $!.message =~ /^Index name 'index_meego_test_sessions_key' on table 'meego_test_sessions' already exists/i
   end
 
   def self.down
