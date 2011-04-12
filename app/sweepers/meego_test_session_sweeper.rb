0e5e85ec7d818ecd65a0a7edd1eb864a97eb09d3
Toni Jyrkinen <toni.jyrkinen@leonidasoy.fi>
2011-04-12 13:31:41 +0300

    use cache sweeper in api import

--- app/sweepers/meego_test_session_sweeper.rb
+++ app/sweepers/meego_test_session_sweeper.rb
@@ -3,10 +3,29 @@ class MeegoTestSessionSweeper < ActionController::Caching::Sweeper
   
   def after_save(test_session)
     expire_cache(test_session)
+    expire_index_for(test_session)
   end
  
   private
    
+
+  def expire_index_for(test_session)
+    expire_page :controller => 'index', :action => :index
+    expire_page :controller => 'upload', :action => :upload_form
+
+    expire_paging_action :controller => "index", :action => "filtered_list", :release_version => test_session.release_version, :target => test_session.target, :testtype => test_session.testtype, :hwproduct => test_session.hwproduct
+    expire_paging_action :controller => "index", :action => "filtered_list", :release_version => test_session.release_version, :target => test_session.target, :testtype => test_session.testtype
+    expire_paging_action :controller => "index", :action => "filtered_list", :release_version => test_session.release_version, :target => test_session.target
+  end
+
+  def expire_fragments_for(test_session)
+  	return if not test_session
+    expire_fragment "preview_page_#{test_session.id}"
+    expire_fragment "view_page_#{test_session.id}"
+    expire_fragment "edit_page_#{test_session.id}"
+    expire_fragment "print_page_#{test_session.id}"
+  end
+
   def expire_cache(test_session)
     expire_fragments_for test_session
 
@@ -20,11 +39,4 @@ class MeegoTestSessionSweeper < ActionController::Caching::Sweeper
 	expire_fragments_for next_session
   end
 
-  def expire_fragments_for(test_session)
-    return if not test_session
-    expire_fragment "preview_page_#{test_session.id}"
-    expire_fragment "view_page_#{test_session.id}"
-    expire_fragment "edit_page_#{test_session.id}"
-    expire_fragment "print_page_#{test_session.id}"
-  end
 end
