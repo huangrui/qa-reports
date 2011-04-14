9e4921337786e6bcacad16880a53d1d3456b7c26
Vesa Poikajärvi <vesa.poikajarvi@digia.com>
2011-04-14 09:52:01 +0300

    List of raw result files shown also in finalize and edit views

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -194,6 +194,8 @@ class ReportsController < ApplicationController
       @hardware = MeegoTestSession.list_hardware @selected_release_version
       @release_versions = MeegoTestSession.release_versions
 
+      @raw_result_files = FileStorage.new(dir="public/reports", baseurl="/reports/").list_report_files(@test_session) or []
+
       render :layout => "report"
     else
       redirect_to :controller => 'upload', :action => :upload_form
@@ -278,6 +280,7 @@ class ReportsController < ApplicationController
       @release_versions = MeegoTestSession.release_versions
       @no_upload_link = true
       @files = FileStorage.new().list_files(@test_session) or []
+      @raw_result_files = FileStorage.new(dir="public/reports", baseurl="/reports/").list_report_files(@test_session) or []
 
       render :layout => "report"
     else
