24de0c67ee9559652267def714d9b5417a7f46f9
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-04-22 09:05:49 +0300

    clarified naming

--- lib/csv_generator.rb
+++ lib/csv_generator.rb
@@ -21,8 +21,8 @@ module CsvGenerator
   def self.generate_csv(release_version, target, testtype, hwproduct)
     sql = <<-END
       select mts.tested_at, vl.label, mts.target, mts.testtype, mts.hwproduct, mts.title,
-        mtset.feature, mtc.name, if(mtc.result = 1,1,0) as passes, if(mtc.result = -1,1,0) as fails,
-        if(mtc.result = 0,1,0) as nas, mtc.comment, author.name, editor.name
+        mtset.feature, mtc.name, if(mtc.result = 1,1,0) as pass, if(mtc.result = -1,1,0) as fail,
+        if(mtc.result = 0,1,0) as na, mtc.comment, author.name, editor.name
       from meego_test_sessions mts
         join users as author on (mts.author_id = author.id)
         join users as editor on (mts.editor_id = editor.id)
@@ -68,9 +68,9 @@ module CsvGenerator
   def self.generate_csv_report(release_version, target, testtype, hwproduct, id)
     sql = <<-END
       select mtset.feature, mtc.name, mtc.comment, 
-        if(mtc.result = 1,1,null) as passes, 
-        if(mtc.result = -1,1,null) as fails,
-        if(mtc.result = 0,1,null) as nas
+        if(mtc.result = 1,1,null) as pass, 
+        if(mtc.result = -1,1,null) as fail,
+        if(mtc.result = 0,1,null) as na
       from meego_test_sets as mtset
         join meego_test_cases as mtc on (mtc.meego_test_set_id = mtset.id)
         join meego_test_sessions as mts on (mtc.meego_test_session_id = mts.id)
