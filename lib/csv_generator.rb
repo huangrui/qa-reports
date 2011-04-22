962afa16b9ae5ea2f883572d04f6b9a99dc5fe8d
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-04-22 09:07:21 +0300

    removed clutter

--- lib/csv_generator.rb
+++ lib/csv_generator.rb
@@ -1,22 +1,3 @@
-#
-# This file is part of meego-test-reports
-#
-# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public License
-# version 2.1 as published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-# 02110-1301 USA
-#
 module CsvGenerator
   def self.generate_csv(release_version, target, testtype, hwproduct)
     sql = <<-END
