huangrui/qa-reports

commit d679d40cef0ec4b5cab7702882bbad554f2da6d8

Refactoring of the release version to separate table

app/helpers/application_helper.rb
@@ -93,6 +93,14 @@ module ApplicationHelper
             html += '<li>'
             link_text = "v#{release}"
         end
+      elsif release =~ /^[A-Za-z][0-9]$/
+        if release.downcase.eql? current_version.downcase
+          html += '<li class="current">'
+          link_text = "MeeGo #{release}"
+        else
+          html += '<li>'
+          link_text = "#{release}"
+        end
       else
         if release == current_version
           html += '<li class="current">'

app/controllers/reports_controller.rb
@@ -229,7 +229,7 @@ class ReportsController < ApplicationController
 
       @test_session = MeegoTestSession.fetch_fully(@report_id)
 
-      return render_404 unless @selected_release_version == @test_session.release_version
+      return render_404 unless @selected_release_version.downcase.eql? @test_session.release_version.downcase
 
       @history = history(@test_session.prev_session)
 

app/controllers/upload_controller.rb
@@ -46,7 +46,7 @@ class UploadController < ApplicationController
     else
       TargetLabel.first_label
     end
-
+    
     @test_session.release_version = if @test_session.release_version.present?
       @test_session.release_version
     elsif default_version.present?

app/models/meego_test_session.rb
@@ -47,6 +47,8 @@ class MeegoTestSession < ActiveRecord::Base
   belongs_to :author, :class_name => "User"
   belongs_to :editor, :class_name => "User"
 
+  belongs_to :version_label, :class_name => "VersionLabel", :foreign_key => "version_label_id"
+
   validates_presence_of :title, :target, :testtype, :hwproduct
   validates_presence_of :uploaded_files, :on => :create
 
@@ -160,28 +162,28 @@ class MeegoTestSession < ActiveRecord::Base
       target    = target.downcase
       testtype  = testtype.downcase
       hwproduct = hwproduct.downcase
-      published.where(:release_version => release_version, :target => target, :testtype => testtype, :hwproduct => hwproduct).order(order_by).limit(limit)
+      published.where("version_labels.normalized" => release_version.downcase, :target => target, :testtype => testtype, :hwproduct => hwproduct).joins(:version_label).order(order_by).limit(limit)
     end
 
     def published_by_release_version_target_test_type(release_version, target, testtype, order_by = "tested_at DESC, id DESC", limit = nil)
       target   = target.downcase
       testtype = testtype.downcase
-      published.where(:release_version => release_version, :target => target, :testtype => testtype).order(order_by).limit(limit)
+      published.where("version_labels.normalized" => release_version.downcase, :target => target, :testtype => testtype).joins(:version_label).order(order_by).limit(limit)
     end
 
     def published_hwversion_by_release_version_target_test_type(release_version, target, testtype)
       target   = target.downcase
       testtype = testtype.downcase
-      published.where(:release_version => release_version, :target => target, :testtype => testtype).select("DISTINCT hwproduct").order("hwproduct")
+      published.where("version_labels.normalized" => release_version.downcase, :target => target, :testtype => testtype).select("DISTINCT hwproduct").joins(:version_label).order("hwproduct")
     end
 
     def published_by_release_version_target(release_version, target, order_by = "tested_at DESC, id DESC", limit = nil)
       target = target.downcase
-      published.where(:release_version => release_version, :target => target).order(order_by).limit(limit)
+      published.where("version_labels.normalized" => release_version.downcase, :target => target).joins(:version_label).order(order_by).limit(limit)
     end
 
     def published_by_release_version(release_version, order_by = "tested_at DESC", limit = nil)
-      published.where(:release_version => release_version).order(order_by).limit(limit)
+      published.where("version_labels.normalized" => release_version.downcase).joins(:version_label).order(order_by).limit(limit)
     end
   end
 
@@ -189,23 +191,23 @@ class MeegoTestSession < ActiveRecord::Base
   # List feature tags                           #
   ###############################################
   def self.list_targets(release_version)
-    (published.all_lowercase(:select => 'DISTINCT target', :conditions=>{:release_version => release_version}).map { |s| s.target.gsub(/\b\w/) { $&.upcase } }).uniq
+    (published.all_lowercase(:select => 'DISTINCT target', :conditions=>{"version_labels.normalized" => release_version}, :include => :version_label).map { |s| s.target.gsub(/\b\w/) { $&.upcase } }).uniq
   end
 
   def self.list_types(release_version)
-    (published.all_lowercase(:select => 'DISTINCT testtype', :conditions=>{:release_version => release_version}).map { |s| s.testtype.gsub(/\b\w/) { $&.upcase } }).uniq
+    (published.all_lowercase(:select => 'DISTINCT testtype', :conditions=>{"version_labels.normalized" => release_version}, :include => :version_label).map { |s| s.testtype.gsub(/\b\w/) { $&.upcase } }).uniq
   end
 
   def self.list_types_for(release_version, target)
-    (published.all_lowercase(:select => 'DISTINCT testtype', :conditions => {:target => target, :release_version => release_version}).map { |s| s.testtype.gsub(/\b\w/) { $&.upcase } }).uniq
+    (published.all_lowercase(:select => 'DISTINCT testtype', :conditions=>{:target => target, "version_labels.normalized" => release_version}, :include => :version_label).map { |s| s.testtype.gsub(/\b\w/) { $&.upcase } }).uniq
   end
 
   def self.list_hardware(release_version)
-    (published.all_lowercase(:select => 'DISTINCT hwproduct', :conditions=>{:release_version => release_version}).map { |s| s.hwproduct.gsub(/\b\w/) { $&.upcase } }).uniq
+    (published.all_lowercase(:select => 'DISTINCT hwproduct', :conditions=>{"version_labels.normalized" => release_version}, :include => :version_label).map { |s| s.hwproduct.gsub(/\b\w/) { $&.upcase } }).uniq
   end
 
   def self.list_hardware_for(release_version, target, testtype)
-    (published.all_lowercase(:select => 'DISTINCT hwproduct', :conditions => {:target => target, :testtype=> testtype, :release_version => release_version}).map { |s| s.hwproduct.gsub(/\b\w/) { $&.upcase } }).uniq
+    (published.all_lowercase(:select => 'DISTINCT hwproduct',  :conditions=>{:target => target, :testtype=> testtype,"version_labels.normalized" => release_version}, :include => :version_label).map { |s| s.hwproduct.gsub(/\b\w/) { $&.upcase } }).uniq
   end
 
 
@@ -218,9 +220,9 @@ class MeegoTestSession < ActiveRecord::Base
 
     # TODO: Works only if there's >= 1s difference between the timestamps
     @prev_session = MeegoTestSession.find(:first, :conditions => [
-        "tested_at < ? AND target = ? AND testtype = ? AND hwproduct = ? AND published = ? AND release_version = ?", time, target.downcase, testtype.downcase, hwproduct.downcase, true, release_version
+        "tested_at < ? AND target = ? AND testtype = ? AND hwproduct = ? AND published = ? AND version_label_id = ?", time, target.downcase, testtype.downcase, hwproduct.downcase, true, version_label_id
     ],
-                          :order              => "tested_at DESC", :include => [
+                          :order => "tested_at DESC", :include => [
          {:meego_test_sets => :meego_test_cases}, :meego_test_sets, :meego_test_cases])
 
     @has_prev = !@prev_session.nil?
@@ -230,9 +232,9 @@ class MeegoTestSession < ActiveRecord::Base
   def next_session
     return @next_session unless @next_session.nil? and @has_next.nil?
     @next_session = MeegoTestSession.find(:first, :conditions => [
-        "tested_at > ? AND target = ? AND testtype = ? AND hwproduct = ? AND published = ? AND release_version = ?", tested_at, target.downcase, testtype.downcase, hwproduct.downcase, true, release_version
+        "tested_at > ? AND target = ? AND testtype = ? AND hwproduct = ? AND published = ? AND version_label_id = ?", tested_at, target.downcase, testtype.downcase, hwproduct.downcase, true, version_label_id
     ],
-                          :order              => "tested_at ASC")
+                          :order => "tested_at ASC")
     @has_next = !@next_session.nil?
     @next_session
   end
@@ -436,6 +438,7 @@ class MeegoTestSession < ActiveRecord::Base
     filename.gsub(/[^\w\.\_\-]/, '_')
   end
 
+<<<<<<< HEAD
   def valid_filename_extension?(filename)
     if filename =~ /\.csv$/i or filename =~ /\.xml$/i
       return true
@@ -443,6 +446,33 @@ class MeegoTestSession < ActiveRecord::Base
       errors.add :uploaded_files, "You can only upload files with the extension .xml or .csv"
       return false
     end
+  end
+
+  ###############################################
+  # For encapsulating the release_version          #
+  ###############################################
+  def release_version=(release_version)
+    @release_version = release_version
+    if release_version
+      version_label = VersionLabel.find(:first, :conditions => {:normalized => release_version.downcase})
+      if version_label
+        self.version_label_id = version_label.id
+      end
+    end
+    if !self.version_label_id
+      @release_version = self.latest_release_version
+      self.version_label_id = VersionLabel.find(:first, :conditions => {:normalized => self.latest_release_version.downcase})
+    end
+  end
+  
+  def release_version
+    if !@release_version 
+      version_label = version_label = VersionLabel.find_by_id(self.version_label_id)
+      if version_label
+        @release_version = version_label.label
+      end
+    end
+    @release_version
   end
 
   def generate_file_destination_path(original_filename)

db/migrate/20110405130041_add_version_label_id_in_meego_test_session.rb
@@ -1,0 +1,8 @@
+class AddVersionLabelIdInMeegoTestSession < ActiveRecord::Migration
+  def self.up
+    add_column :meego_test_sessions, :version_label_id, :integer, :null => false, :default => 0
+  end
+  def self.down
+    remove_column :meego_test_sessions, :version_label_id
+  end
+end

db/migrate/20110406130041_migrate_release_version_to_version_label_id_in_meego_test_session.rb
@@ -1,0 +1,22 @@
+class MigrateReleaseVersionToVersionLabelIdInMeegoTestSession < ActiveRecord::Migration
+  def self.up
+    #add_column :meego_test_sessions, :version_label_id, :integer, :null => false, :default => 1
+    
+    sessions = MeegoTestSession.find(:all)
+    targets = VersionLabel.find(:all)
+    sessions.each do |s|
+      targets.each do |t|
+        if s.release_version == t.label
+          s.version_label_id = t.id
+        end
+      end
+      s.save()
+    end
+
+    remove_column :meego_test_sessions, :release_version
+  end
+
+  def self.down
+    remove_column :meego_test_sessions, :version_label_id
+  end
+end

features/step_definitions/authentication_steps.rb
@@ -50,7 +50,7 @@ Then /^I should return to report "([^\"]*)" and see "([^\"]*)" and a "Sign out" 
   # TODO: DRY
   version, target, test_type, hardware = report_string.downcase.split('/')
   report = MeegoTestSession.first(:conditions =>
-   {:target => target, :hwproduct => hardware, :testtype => test_type, :release_version => version}
+   {:target => target, :hwproduct => hardware, :testtype => test_type, "version_labels.normalized" => version}, :include => :version_label 
   )
   report.should_not be_nil
 

features/step_definitions/custom_web_steps.rb
@@ -14,7 +14,7 @@ Then /^the link "([^"]*)" within "([^"]*)" should point to the report "([^"]*)"/
 
     version, target, test_type, hardware = expected_report.downcase.split('/')
     report = MeegoTestSession.first(:conditions =>
-     {:release_version => version, :target => target, :hwproduct => hardware, :testtype => test_type}
+     {"version_labels.normalized" => version, :target => target, :hwproduct => hardware, :testtype => test_type}, :include => :version_label
     )
     raise "report not found with parameters #{version}/#{target}/#{hardware}/#{test_type}!" unless report
 

commit a681bae6cd0f26a4fa48bcdf94c3034ad52df5f2

Fided index page performance problem introduced in d679d40cef0ec4b5cab7702882bbad554f2da6d8

app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < ActionController::Base
   end
 
   def get_selected_release_version
-    @selected_release_version = params[:release_version] || MeegoTestSession.latest_release_version
+    @selected_release_version = params[:release_version] || VersionLabel.latest.label
   end
 
   def render_404

app/controllers/index_controller.rb
@@ -21,18 +21,15 @@
 # 02110-1301 USA
 #
 require 'graph'
+
 class IndexController < ApplicationController
   include Graph
-  
-  #caches_page :index, :filtered_list
   caches_action :filtered_list, :layout => false, :expires_in => 1.hour
 
   def index
-    @types = {}
-    MeegoTestSession::targets.each{|target|
-      @types[target] = MeegoTestSession.list_types_for @selected_release_version, target
-    }
-    @hardware = MeegoTestSession.list_hardware @selected_release_version
+    @hardwares = Hardware.by_profile_by_test_type(@selected_release_version)
+
+    @profiles = TargetLabel.targets
     @target = params[:target]
     @testtype = params[:testtype]
     @hwproduct = params[:hwproduct]

app/helpers/application_helper.rb
@@ -81,6 +81,7 @@ module ApplicationHelper
    html.html_safe
  end
 
+  # FIXME: Cleanup with link_to_unless_current
   def release_version_navigation(current_version, target='', testtype='', hwproduct='')
     html = '<ul class="clearfix">'
     link_text = ''

app/models/hardware.rb
@@ -1,0 +1,25 @@
+class Hardware
+
+  def self.by_profile_by_test_type(release)
+    selection = <<-END
+      DISTINCT target as profile, testtype, hwproduct as hardware
+    END
+
+    @hardwares = MeegoTestSession.published.select(selection).joins(:version_label).
+      where(:version_labels => {:normalized => release.downcase})
+
+    @hardwares = @hardwares.group_by(&:profile)
+
+    @hardwares.each do |profile, hardwares_by_profile|
+      @hardwares[profile] = hardwares_by_profile.group_by(&:testtype)
+    end
+
+    @hardwares.each do |profile, hardwares|
+      @hardwares[profile].each do |testtype, hardwares|
+        @hardwares[profile][testtype] = hardwares.map(&:hardware)
+      end
+    end
+
+    @hardwares
+  end
+end

app/models/target_label.rb
@@ -1,4 +1,9 @@
 class TargetLabel < ActiveRecord::Base
+
+  def self.targets
+    order("sort_order ASC").select(:label).map(&:label)
+  end  
+    
   def self.labels
     find(:all, :select => "normalized").map(&:normalized)
   end

app/models/version_label.rb
@@ -1,5 +1,12 @@
 class VersionLabel < ActiveRecord::Base
+
+  scope :in_sort_order, order("sort_order ASC")
+
   def self.versions
     find(:all, :select => "normalized").map(&:normalized)
+  end
+
+  def self.latest
+    in_sort_order.first
   end
 end

app/views/index/_profile.html.slim
@@ -1,0 +1,15 @@
+td
+  - if @hardwares[profile.downcase]
+    - @hardwares[profile.downcase].each do |testtype, hardwares|
+
+      - comparetype = testtype+":Testing"
+      - compare = MeegoTestSession::published_hwversion_by_release_version_target_test_type(@selected_release_version, profile, comparetype).first()
+
+      strong
+        = link_to testtype, test_type_report_path(@selected_release_version, profile, testtype)
+      - if compare then
+        a.compare href=url_for(:controller => 'reports', :action => 'compare', :release_version=>@selected_release_version, :target=>profile, :testtype=>testtype, :comparetype=>comparetype) compare
+      ul
+        - hardwares.each do |hw|
+          li
+            = link_to hw.humanize, hardware_report_path(@selected_release_version, profile, testtype, hw.humanize)

app/views/index/_testtype.html.slim
@@ -1,14 +1,0 @@
-/ TODO: comparetype and compare to be given by controller
-- hardware = hardware_for(target, testtype)
-- comparetype = testtype+":Testing"
-- compare = MeegoTestSession::published_hwversion_by_release_version_target_test_type(@selected_release_version, target, comparetype).first()
-
-- if hardware.present?
-  strong
-    a href=filtered_index_url(target, testtype) =testtype
-  - if compare then
-    a.compare href=url_for(:controller => 'reports', :action => 'compare', :release_version=>@selected_release_version, :target=>target, :testtype=>testtype, :comparetype=>comparetype) compare
-  ul
-    - hardware.each do |hw|
-      li
-        a href=url_for(:controller=>'index', :action=>'filtered_list', :release_version=>@selected_release_version, :target=>target, :testtype=>testtype, :hwproduct=>hw) =hw

app/views/index/_title.html.slim
@@ -1,0 +1,2 @@
+th scope="col"
+  = link_to profile, profile_report_path(@selected_release_version, profile)

app/views/index/index.html.slim
@@ -6,11 +6,7 @@
   table#report_navigation
     thead
       tr
-        - MeegoTestSession::targets.each do |target|
-          th scope="col"
-            a href=filtered_index_url(target) =target
+        = render :partial => "title", :collection => @profiles, :as => :profile
 
     tr
-      - MeegoTestSession::targets.each do |target|
-        td
-          = render :partial=>'testtype', :collection=>@types[target], :locals => {:target=>target}
+      = render :partial => "profile", :collection => @profiles

config/routes.rb
@@ -64,9 +64,9 @@ Meegoqa::Application.routes.draw do
     match '/:release_version/:target/:testtype/:hwproduct/:id/delete' => 'reports#delete', :via => "post"
     match '/:release_version/:target/:testtype/:hwproduct/:id/print' => 'reports#print', :via => "get"
 
-    match '/:release_version/:target/:testtype/:hwproduct' => 'index#filtered_list', :via => "get"
-    match '/:release_version/:target/:testtype' => 'index#filtered_list', :via => "get"
-    match '/:release_version/:target' => 'index#filtered_list', :via => "get"
+    match '/:release_version/:target/:testtype/:hwproduct' => 'index#filtered_list', :via => "get", :as => :hardware_report
+    match '/:release_version/:target/:testtype' => 'index#filtered_list', :via => "get", :as => :test_type_report
+    match '/:release_version/:target' => 'index#filtered_list', :via => "get", :as => :profile_report
     match '/:release_version' => 'index#index', :via => "get"
   end
 