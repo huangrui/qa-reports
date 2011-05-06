2462f9a4d6fd5aa0e6c0a3621a38a31e3b91a87c
Toni Jyrkinen <toni.jyrkinen@leonidasoy.fi>
2011-05-06 12:30:37 +0300

    meego test cases controller returns the test case comment html on its update

--- app/controllers/meego_test_cases_controller.rb
+++ app/controllers/meego_test_cases_controller.rb
@@ -6,14 +6,17 @@ class MeegoTestCasesController < ApplicationController
   def update_case_comment
     case_id  = params[:id]
     comment  = params[:comment]
+    attachment = params[:attachment]
     testcase = MeegoTestCase.find(case_id)
     testcase.update_attribute(:comment, comment)
+    testcase.update_attachment(attachment)
 
-    test_session = testcase.meego_test_session
-    test_session.updated_by(current_user)
-    expire_caches_for(test_session)
+    @test_session = testcase.meego_test_session
+    @test_session.updated_by(current_user)
+    @editing = true
+    expire_caches_for(@test_session)
 
-    render :text => "OK"
+    logger.info(render :partial => 'reports/testcase_comment', :locals => {:testcase => testcase})
   end
 
   def update_case_result
