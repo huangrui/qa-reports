154e7ac804183fe567f1c2dd4119ac3d0063990b
Toni Jyrkinen <toni.jyrkinen@leonidasoy.fi>
2011-05-09 15:24:17 +0300

    renamed TC image to attachment

--- app/models/meego_test_case_attachment.rb
+++ app/models/meego_test_case_attachment.rb
@@ -2,7 +2,7 @@ class MeegoTestCaseAttachment < ActiveRecord::Base
 	belongs_to :meego_test_case
 	validates_presence_of :meego_test_case
 
-	has_attached_file :image
+	has_attached_file :attachment
     #, :url => "/attachments/:basename.:extension?:id"
     #, :path => ":rails_root/public/system/:attachment/:id/:style/:filename"
 end
