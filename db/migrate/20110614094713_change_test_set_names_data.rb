e399659c1b3f5e4af362dc5669a80aaa55774da2
Toni Jyrkinen <toni.jyrkinen@leonidasoy.fi>
2011-06-14 14:57:54 +0300

    data migration to unify several test set names

---
+++ db/migrate/20110614094713_change_test_set_names_data.rb
@@ -0,0 +1,50 @@
+class ChangeTestSetNamesData < ActiveRecord::Migration
+
+  def self.up
+    mappings = {
+      'acceptance-auto-test' => {:test_set => 'Acceptance'},
+      'acceptance for developer edition' => {:test_set => 'Acceptance', :hw_suffix => '_CE'},
+      'basic feature testing' => {:test_set => 'Basic Feature'},
+      'data flow for developer edition' =>  {:test_set => 'Dataflow', :hw_suffix => '_CE'},
+      'de-feature' => {:test_set => 'Key Feature', :hw_suffix => '_CE'},
+      'de-sanity' => {:test_set => 'Sanity', :hw_suffix => '_CE'},
+      'full feature testing' =>  {:test_set => 'Extended Feature'},
+      'fullpass' =>  {:test_set => 'Milestone'},
+      'key basic feature' =>  {:test_set => 'Basic Feature'},
+      'key basic feature for developer edition' =>  {:test_set => 'Basic Feature', :hw_suffix => '_CE'},
+      'sanity-auto-test' => {:test_set => 'Sanity'},
+      'sanity for developer edition' =>  {:test_set => 'Sanity', :hw_suffix => '_CE'},
+      'system function' =>  {:test_set => 'System Functional'},
+      'update system function' =>  {:test_set => 'System Functional'},
+      'update sanity' =>  {:test_set => 'Sanity'},
+      'updated sanity' =>  {:test_set => 'Sanity'},
+      'use case testing for developer edition' =>  {:test_set => 'Key Feature', :hw_suffix => '_CE'},
+      'winxp 32bit ia32 basic' =>  {:test_set => 'Basic Feature'}
+    }
+
+    MeegoTestSession.where(:testtype => mappings.keys).each do |s|
+      mapping = mappings[s.testtype.downcase]
+      s.testtype = mapping[:test_set]
+      hw_suffix = mapping[:hw_suffix]
+      s.hardware += hw_suffix unless hw_suffix.nil?
+      s.save
+    end
+
+    MeegoTestSession.where(:hardware => 'N900de').each do |s|
+      s.hardware = 'N900_CE'
+      s.save
+    end
+
+    # Not to be processed:
+    #'Sanity-automated' => {:test_set => 'Sanity'},
+    #'Sanity:testing' => {:test_set => 'Sanity:testing'},
+    #'Sanity - automated' =>  {:test_set => 'Sanity'},
+    #'Hourly - automated' =>  {:test_set => ' (have no idea which TestSet it belongs to, Sanity?)'},
+    #'Nightly-automated' =>  {:test_set => ' (have no idea which TestSet it belongs to, Acceptance?)'},
+    #'Sample report' =>  {:test_set => ' (have no idea which TestSet it belongs to, System functional?)'},
+    #'Installation' =>  {:test_set => ''},
+  end
+
+  def self.down
+  end
+end
