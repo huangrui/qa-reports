7b5b8006da0b8dd5689614f174f5f7e42fa85a38
Huang Rui <rui.r.huang@intel.com>
2011-07-05 12:53:46 +0800

    Add the configuration of the internal server
    Add the change of testset and hardware for the internal server
    
    Signed-off-by: Huang Rui <rui.r.huang@intel.com>

--- db/migrate/20110628093441_rename_hardware_and_testype_data_case_sensitively.rb
+++ db/migrate/20110628093441_rename_hardware_and_testype_data_case_sensitively.rb
@@ -1,7 +1,8 @@
 class RenameHardwareAndTestypeDataCaseSensitively < ActiveRecord::Migration
   def self.up
     # a little house-keeping while we're at it
-    MeegoTestSession.delete_all "published = false AND created_at < '#{Time.local 2011, 6, 28}'"
+    # Intel internal server: For hide-obsolete-reports feature, it's unable to delete the sessions of "published = false". So mark it as comment.
+    #MeegoTestSession.delete_all "published = false AND created_at < '#{Time.local 2011, 6, 28}'"
 
     hardware_mapping = {
       'ia aava' => 'IA-Aava',
@@ -29,7 +30,27 @@ class RenameHardwareAndTestypeDataCaseSensitively < ActiveRecord::Migration
       'exopc-all' => 'ExoPC-All',
       'netbook-all' => 'Netbook-All',
       'netbook-eeepc1005pe-all' => 'Netbook-EeePC1005PE-All',
-      'tablet-exopc-all' => 'Tablet-ExoPC-All'
+      'tablet-exopc-all' => 'Tablet-ExoPC-All',
+      # Add the hardware mapping
+      'acer ff 7"' => 'Acer MM 7"',
+      'acer ff a2' => 'Acer MM 7"',
+      'acer ff b1' => 'Acer MM 7"',
+      'exopc' => 'ExoPC',
+      'pinetrail oss + extras - exopc' => 'Pinetrail OSS + Extras - ExoPC',
+      'pinetrail oss + extras - lenovo' => 'Pinetrail OSS + Extras - Lenovo',
+      'pinetrail oss - lenovo' => 'Pinetrail OSS - Lenovo',
+      'pinetrail oss - exopc' => 'Pinetrail OSS - ExoPC',
+      'acer_tablet' => 'Acer_Tablet',
+      'pinetrail oss + extras' => 'Pinetrail OSS + Extras',
+      'Pinetrail_tablet' => 'Pinetrail_Tablet',
+      'acer ff b2' => 'Acer MM 7"',
+      'acer ff 10"' => 'Acer MM 10"',
+      'netbook-daily-1-2testing' => 'Netbook-Daily-1-2Testing',
+      'tablet-exopc-daily-1-2testing' => 'Tablet-ExoPC-Daily-1-2Testing',
+      'netbook-daily-1-3testing' => 'Netbook-Daily-1-3Testing',
+      'acer mm 10"' => 'Acer MM 10"',
+      'acer 10"' => 'Acer MM 10"',
+      'tablet-exopc-daily-1-2trunk' => 'Tablet-ExoPC-Daily-1-2Trunk'
     }
 
     testtype_mapping = {
@@ -50,6 +71,11 @@ class RenameHardwareAndTestypeDataCaseSensitively < ActiveRecord::Migration
       'milestone' => 'Milestone',
       'functional' => 'Functional',
       'auto' => 'Auto',
+      # Add the testset mapping
+      'ux e2e user test' => 'UX E2E User Test',
+      'auto_acer_test' => 'Auto_Acer_Test',
+      'bat_pinetraill_oss' => 'Bat_Pinetraill_OSS',
+      'abat_tablet' => 'Abat_Tablet'
     }
 
     # map unlisted hardwares as 'hardware name' => 'Hardware Name'
