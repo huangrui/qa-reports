87bc2a9e101c29fc2e89cb0d2d4a7122fc1fdede
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-07-12 12:41:30 +0300

    Parse FuTe results from xml

--- lib/result_file_parser.rb
+++ lib/result_file_parser.rb
@@ -1,7 +1,25 @@
 module ResultFileParser
 
   def self.parse_xml(io)
+    test_cases = {}
 
+    doc = Nokogiri::XML(io) { |config| config.strict }
+    sets = doc.css('set')
+
+    sets.each do | set |
+      feature = set['feature'] || ""
+
+      set.css('case').each do |test_case|
+        name    = test_case['name'] || ""
+        comment = test_case['comment'] || ""
+        result  = parse_xml_test_case_result(test_case['result'])
+
+        test_cases[feature] ||= {}
+        test_cases[feature][name] = {:name => name, :result => result, :comment => comment}
+      end
+    end
+
+    test_cases
   end
 
   def self.parse_csv(io)
@@ -13,7 +31,7 @@ module ResultFileParser
       feature = row[0].toutf8.strip
       name    = row[1].toutf8.strip
       comment = row[2] ? row[2].toutf8.strip : ""
-      result  = parse_test_case_result(row[3], row[4], row[5])
+      result  = parse_csv_test_case_result(row[3], row[4], row[5])
 
       test_cases[feature] ||= {}
       test_cases[feature][name] = {:name => name, :result => result, :comment => comment}
@@ -24,7 +42,18 @@ module ResultFileParser
 
   private
 
-  def self.parse_test_case_result(pass, fail, na)
+  def self.parse_xml_test_case_result(result)
+    result_mapping = { 
+      "PASS" => MeegoTestCase::PASS,
+      "FAIL" => MeegoTestCase::FAIL,
+      "NA"   => MeegoTestCase::NA,
+      nil     => MeegoTestCase::NA
+    }
+
+    result_mapping[result]
+  end
+
+  def self.parse_csv_test_case_result(pass, fail, na)
     if pass == "1" && fail != "1" && na !="1"
       result = MeegoTestCase::PASS
     elsif pass != "1" && fail == "1" && na !="1"
