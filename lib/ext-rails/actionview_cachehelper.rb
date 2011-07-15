d33fcca2c1352022835088c808de0f357b649685
Lauri Hahne <lauri.hahne@leonidasoy.fi>
2011-07-15 10:41:47 +0300

    updated the CacheHelper fix to a newer and bug-free version

--- lib/ext-rails/actionview_cachehelper.rb
+++ lib/ext-rails/actionview_cachehelper.rb
@@ -13,12 +13,12 @@ module ActionView
           # This dance is needed because Builder can't use capture
           pos = output_buffer.length
           yield
-          if output_buffer.is_a?(ActionView::OutputBuffer)
+          if output_buffer.html_safe?
             safe_output_buffer = output_buffer.to_str
             fragment = safe_output_buffer.slice!(pos..-1)
             self.output_buffer = ActionView::OutputBuffer.new(safe_output_buffer)
           else
-            fragment = output_buffer.slice(pos..-1) #modified here from 'slice!'
+            fragment = output_buffer.slice!(pos..-1)
           end
           controller.write_fragment(name, fragment, options)
         end
