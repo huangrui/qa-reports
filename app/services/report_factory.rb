c53b0564176b1ff86664c37de740793723e7db1a
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-08-05 13:53:04 +0300

    Remove unnecessary code

--- app/services/report_factory.rb
+++ app/services/report_factory.rb
@@ -14,7 +14,6 @@ class ReportFactory
       save_result_files(params)
 
       test_session = MeegoTestSession.new(params)
-      build_test_case_associations(test_session)
       copy_template_values(test_session)
 
     rescue ParseError => e
@@ -94,13 +93,6 @@ class ReportFactory
 
   def sanitize_filename(filename)
     filename.gsub(/[^\w\.\_\-]/, '_')
-  end
-
-  def build_test_case_associations(test_session)
-    #TODO: This association could be thrown away and navigated through features
-    test_session.features.each do |feature|
-      feature.meego_test_cases.each { |tc| tc.meego_test_session = test_session }
-    end
   end
 
   def copy_template_values(test_session)
