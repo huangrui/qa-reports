8c0cc334fb8d71a0418be0af80a4234009291507
Vesa Poikajärvi <vesa.poikajarvi@digia.com>
2011-08-16 14:33:46 +0300

    Tidyed NFT report updating feature

--- features/step_definitions/api_update_cases_steps.rb
+++ features/step_definitions/api_update_cases_steps.rb
@@ -1,9 +1,20 @@
+
+
+
 When /^the client sends a updated file "([^\"]*)" with the id (\d+) via the REST API$/ do |file, report_id|
   report_id = MeegoTestSession.find(:first).id
   post "/api/update/#{report_id}?auth_token=foobar", {
       "report"          => Rack::Test::UploadedFile.new("features/resources/#{file}", "text/xml")
   }
   response.should be_success
+end
+
+When "the client sends an updated result file" do
+  When %{the client sends a updated file "sim.xml" with the id 1 via the REST API}
+end
+
+And "I have sent a file with NFT results" do
+  And %{I have sent the file "serial_result.xml" via the REST API}
 end
 
 And /^I have sent the file "([^\"]*)" via the REST API$/ do |file|
@@ -32,4 +43,18 @@ When /^the client sends 1 updated valid file, and 1 invalid file with the id (\d
       "report.2"        => Rack::Test::UploadedFile.new("features/resources/invalid.xml", "text/xml"),
   }
   response.should be_success
+end
+
+When /^I view the [\w\ ]*report$/ do
+ When %{I view the report "1.2/Netbook/Automated/N900"}
+end
+
+Then "I see NFT results" do
+  Then %{I should find element "#detailed_nft_results"}
+  And %{I should find element "a[href='#detailed_nft_results']" within ".toc"}
+end
+
+Then "I should not see NFT results" do
+  Then %{I should not find element "#detailed_nft_results"}
+  And %{I should not find element "a[href='#detailed_nft_results']" within ".toc"}
 end
