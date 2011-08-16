ee418545b6fe6cf27ab0bb45513781f917e7eaa9
Vesa Poikajärvi <vesa.poikajarvi@digia.com>
2011-08-16 15:05:06 +0300

    Update API cases

--- features/step_definitions/api_update_cases_steps.rb
+++ features/step_definitions/api_update_cases_steps.rb
@@ -8,11 +8,31 @@ When /^the client sends a updated file "([^\"]*)" with the id (\d+) via the REST
 end
 
 When "the client sends an updated result file" do
-  When %{the client sends a updated file "sim.xml" with the id 1 via the REST API}
+  When %{the client sends a updated file "sim_new.xml" with the id 1 via the REST API}
+end
+
+When "the client sends an updated but invalid result file" do
+  When %{the client sends a updated file "invalid.xml" with the id 1 via the REST API}
+end
+
+When "the client sends an updated file with invalid extension" do
+  When %{the client sends a updated file "invalid_ext.txt" with the id 1 via the REST API}
 end
 
 When "I have sent a file with NFT results" do
   When %{the client sends file "features/resources/serial_result.xml"}
+end
+
+When "I have sent a basic result file" do
+  When %{the client sends a basic test result file}
+end
+
+When "the client sends several updated files" do
+  When %{the client sends several updated files with the id 1 via the REST API}
+end
+
+When "the client sends a valid and an invalid file" do
+  When %{the client sends 1 updated valid file, and 1 invalid file with the id 1 via the REST API}
 end
 
 When /^the client sends several updated files with the id (\d+) via the REST API$/ do |report_id|
