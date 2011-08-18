c870a74db72d8faa2af33b2d78b085b79d4c499c
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-08-18 11:32:55 +0300

    cleanup after review

--- app/controllers/test_cases_controller.rb
+++ app/controllers/test_cases_controller.rb
@@ -6,12 +6,13 @@ class TestCasesController < ApplicationController
   after_filter  :update_report_editor
 
   def update
-    #TODO: Doesn't check if the update fails
     #TODO: AttachmentsController should take care of attachments
     #TODO: Ugly hack to maintain current "delete attachment" functionality
     params[:test_case][:attachment] = params[:test_case][:attachment] if params[:test_case][:comment]
 
     @test_case = MeegoTestCase.unscoped.find(params[:id])
+
+    #TODO: Doesn't check if the update fails
     @test_case.update_attributes(params[:test_case])
 
     #TODO: Canonical way of doing response: head :ok
