9cf36acb1c247fca7322712965351affb59d27b7
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-08-22 14:58:51 +0300

    Use bundler while executing rake

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,6 +10,6 @@ namespace :db do
   desc "Import production database to staging"
   task :import, :roles => :db, :only => {:primary => true} do
     upload "./qa_reports_production.sql.bz2", "#{current_path}/qa_reports_production.sql.bz2"
-    run "cd #{current_path} && RAILS_ENV='#{rails_env}' rake db:import_to_db"
+    run "cd #{current_path} && RAILS_ENV='#{rails_env}' bundle exec rake db:import_to_db"
   end
 end
