133130abe51ca42ad55ae8097142e10dd46180ae
Tomi Laine <tomi.laine@leonidasoy.fi>
2011-08-23 12:48:42 +0300

    fixed staging deployment script

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -11,6 +11,7 @@ namespace :db do
   task :import, :roles => :db, :only => {:primary => true} do
     upload "./qa_reports_production.sql.bz2", "#{current_path}/qa_reports_production.sql.bz2"
     run "cd #{current_path} && RAILS_ENV='#{rails_env}' bundle exec rake db:import_to_db"
+  end
 
   desc "Import files to staging"
   task :import_files, :roles => :db, :only => {:primary => true} do
