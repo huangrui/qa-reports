503c983e154ae5b5392614c884effbf9307721d5
Jarkko Mönkkönen <jarkko.monkkonen@leonidasoy.fi>
2011-08-23 13:59:58 +0300

    Use cache sweeper. Fix update_report_editor to work and return head :ok instead of "OK".

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,6 +1,6 @@
 class FeaturesController < ApplicationController
   include CacheHelper
-  # cache_sweeper :meego_test_session_sweeper
+  cache_sweeper :meego_test_session_sweeper
 
   before_filter :authenticate_user!
   after_filter  :update_report_editor
@@ -8,15 +8,13 @@ class FeaturesController < ApplicationController
   def update
     @feature = Feature.find(params[:id])
     @feature.update_attributes(params[:feature]) # Doesn't check for failure
-
-    #TODO: Canonical way of doing response: head :ok
-    render :text => "OK"
+    head :ok
   end
 
   private
 
   def update_report_editor
-    report = @test_case.meego_test_session
+    report = @feature.meego_test_session
     report.update_attribute(:editor, current_user)
   end
 end
