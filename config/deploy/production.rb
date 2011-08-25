5f89aecc9318bed5ed0d0052a4ad98ef00c41ffb
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-08-25 18:41:41 +0300

    rsync uploaded files from production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,6 +5,7 @@ set :rails_env, "production"
 ssh_options[:port] = 43398
 
 server "qa-reports.meego.com", :app, :web, :db, :primary => true
+host = roles[:db].servers.first.host
 
 after "deploy:symlink" do
   # Allow robots to index qa-reports.meego.com
@@ -13,11 +14,12 @@ after "deploy:symlink" do
 end
 
 namespace :db do
-  desc "Dump and fetch production database"
+  desc "Dump and fetch production database and uploaded files"
   task :dump, :roles => :db, :only => {:primary => true} do
     run "cd #{current_path} && RAILS_ENV='#{rails_env}' bundle exec rake db:dump"
     get "#{current_path}/qa_reports_production.sql.bz2", "./qa_reports_production.sql.bz2"
     run "rm #{current_path}/qa_reports_production.sql.bz2"
+    `rsync --rsh="ssh -p #{ssh_options[:port]}" -RLrvaz #{user}@#{host}:#{current_path}/public/files/attachments public/files/attachment`
   end
 
   desc "Compress and fetch files"
