8b5d5ad3efabb76604a9c5bdb9c6c926aa8b8991
Tomi Laine <tomi.laine@leonidasoy.fi>
2011-08-26 10:37:08 +0300

    fixed print and show report

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Meegoqa::Application.routes.draw do
   devise_for :users, :controllers => { :sessions => "users/sessions" }  , :path_names => { :sign_up => "#{DeviseRegistrationConfig::URL_TOKEN}/register" }
 
-  resources :reports,     :only => [:update, :show, :edit] do
-    member do
-      get 'print'
-    end
-  end
+  resources :reports,     :only => [:update, :show, :edit]
 
   resources :features,    :only => [:update]
   resources :test_cases,  :only => [:update]
@@ -55,11 +51,11 @@ Meegoqa::Application.routes.draw do
 
     match '/:release_version/:target(/:testset(/:product))/report_list(/:page)' => 'report_groups#report_page', :via => "get", :as => :report_list
 
-    match '/:release_version/:target/:testset/:product/:id' => 'reports#show', :via => "get"
+    match '/:release_version/:target/:testset/:product/:id' => 'reports#show', :via => "get", :as => :show_report
     match '/:release_version/:target/:testset/:product/:id/edit' => 'reports#edit', :via => "get"
     match '/:release_version/:target/:testset/:product/:id/download' => 'csv_export#export_report', :via => "get"
     match '/:release_version/:target/:testset/:product/:id/delete' => 'reports#delete', :via => "post"
-    match '/:release_version/:target/:testset/:product/:id/print' => 'reports#print', :via => "get"
+    match '/:release_version/:target/:testset/:product/:id/print' => 'reports#print', :via => "get", :as => :print_report
 
     match '/:release_version' => 'index#index', :via => "get"
 
