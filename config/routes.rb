e94928f3b368f7ea1873e041b6fc1ce2109e974d
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-08-27 07:40:46 +0300

    cleanup for routes

--- config/routes.rb
+++ config/routes.rb
@@ -47,8 +47,8 @@ Meegoqa::Application.routes.draw do
     match '/:release_version/:target/:testset/compare/:comparetestset'  => 'comparison_reports#show',  :via => "get", :as => :branch_comparison
     match '/:release_version(/:target(/:testset(/:product)))/rss'       => 'rss#rss',                  :via => "get"
 
-    match '/:release_version(/:target(/:testset(/:product)))'                   => 'report_groups#show',        :via => "get", :as => :group_report
     match '/:release_version/:target(/:testset(/:product))/report_list(/:page)' => 'report_groups#report_page', :via => "get", :as => :report_list
+    match '/:release_version(/:target(/:testset(/:product)))'                   => 'report_groups#show',        :via => "get", :as => :group_report
   end
 
   root :to => "index#index"
