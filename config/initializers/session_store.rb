2aef62ec1a0cfe1bc76a0a2f6013ff6006be47b8
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-08-30 13:09:48 +0300

    Save sessions in database

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,14 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Meegoqa::Application.config.session_store :cookie_store, :key => 'meego-qa-reports'
-
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# Meegoqa::Application.config.session_store :active_record_store
-
-Rails.application.config.middleware.insert_before(
-  ActionDispatch::Session::CookieStore,
-  FlashSessionCookieMiddleware,
-  Rails.application.config.session_options[:key]
-)
+Meegoqa::Application.config.session_store :active_record_store, :key => 'meego-qa-reports'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110829092127) do
+ActiveRecord::Schema.define(:version => 20110830094934) do
 
   create_table "features", :force => true do |t|
     t.string  "name",                  :default => ""
@@ -107,6 +107,16 @@ ActiveRecord::Schema.define(:version => 20110829092127) do
   end
 
   add_index "serial_measurements", ["meego_test_case_id"], :name => "index_serial_measurements_on_meego_test_case_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "target_labels", :force => true do |t|
     t.string  "label",      :limit => 64, :null => false
