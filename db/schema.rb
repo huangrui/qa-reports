9940605703e6b086fa855ff86c009f920b37bc4a
Toni Jyrkinen <toni.jyrkinen@leonidasoy.fi>
2011-08-31 14:48:15 +0300

    migration to move fileattachments to directories by their testreport id's

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110830094934) do
+ActiveRecord::Schema.define(:version => 20110831095400) do
 
   create_table "features", :force => true do |t|
     t.string  "name",                  :default => ""
