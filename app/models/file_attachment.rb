cc5fe085b85d6cd4c0303db73353d19d4cd2b031
Toni Jyrkinen <toni.jyrkinen@leonidasoy.fi>
2011-09-01 15:07:56 +0300

    FileAttachment correctly relocates its file after save when its parent id has changed

--- app/models/file_attachment.rb
+++ app/models/file_attachment.rb
@@ -5,8 +5,20 @@ class FileAttachment < ActiveRecord::Base
 
   delegate :url, :to => :file
 
+  after_find { @original_path = file.path }
+  after_save :relocate_file
+
   Paperclip.interpolates :attachable_id do |attachment, style|
-    attachment.instance.attachable_id
+    attachment.instance.attachable_id || 'unassigned'
+  end
+
+  def relocate_file
+    return unless @original_path && @original_path != file.path
+
+    Rails.logger.info "[file_attachment] moving #{@original_path} to #{file.path}"
+    FileUtils.mkpath File.dirname(file.path)
+    FileUtils.mv @original_path, file.path
+    FileUtils.rmdir File.dirname(@original_path)
   end
 
   def image?
