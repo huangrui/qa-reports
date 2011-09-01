7fe149ef8923152a9a6d982a494e2fcfa041564a
Vesa Poikajärvi <vesa.poikajarvi@iki.fi>
2011-09-01 18:39:52 +0300

    Added forgotten field

--- lib/csv_generator.rb
+++ lib/csv_generator.rb
@@ -1,4 +1,9 @@
 module CsvGenerator
+  # The SQL queries do a LEFT JOIN on measurements. If there are more than
+  # one measurement per test case the output will contain multiple rows per
+  # test case. This is intended - most likely the users of CSV export don't
+  # have such cases.
+
   CSV_REPORT_QUERY = <<-END
     SELECT 
     mtset.name                 AS feature, 
@@ -113,10 +118,10 @@ module CsvGenerator
 
       result.each do |row|
         csv << [row[:tested_at], row[:release_version], row[:target], 
-                row[:product], row[:session], row[:feature], row[:testcase], 
-                row[:pass], row[:fail], row[:na], row[:comment], row[:m_name],
-                row[:m_value], row[:m_unit], row[:m_target], row[:m_failure],
-                row[:author], row[:editor]]
+                row[:target], row[:product], row[:session], row[:feature], 
+                row[:testcase], row[:pass], row[:fail], row[:na], 
+                row[:comment], row[:m_name], row[:m_value], row[:m_unit], 
+                row[:m_target], row[:m_failure], row[:author], row[:editor]]
       end
     end
   end
