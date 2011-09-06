81c86641c56a0351f9b287c4eb516e3af02ef2b8
Tomi Laine <tomi.laine@leonidasoy.fi>
2011-09-06 13:29:35 +0300

    fixed result file creation in report_steps

--- features/step_definitions/report_steps.rb
+++ features/step_definitions/report_steps.rb
@@ -15,8 +15,9 @@ Bluetooth,Test Case 2,,0,1,0'
 
   tmp = Tempfile.new('result_file')
   tmp << RESULT_CSV
+  file = ActionDispatch::Http::UploadedFile.new(:tempfile => tmp, :filename => 'result.csv')
   report_attributes = MeegoTestSession.first.attributes.merge(:tested_at => '2011-09-02')
-  report_attributes[:uploaded_files] = [ActionDispatch::Http::UploadedFile.new(:tempfile => tmp, :filename => 'result.csv')]
+  report_attributes[:result_files_attributes] = [{:file => file, :attachment_type => :result_file}]
 
   report = ReportFactory.new.build(report_attributes)
   report.save!
