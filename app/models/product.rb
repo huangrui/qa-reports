18621c9bad98040371ca64df00e4e582276217dd
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-09-13 12:57:11 +0300

    Cleanup the index page

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,20 @@ class Product
 
   def self.by_profile_by_testset(release)
     selection = "DISTINCT target as profile, testset, product"
-    products  = MeegoTestSession.published.select(selection).release(release.name).order(:target, :testset, :product)
-    products.to_nested_hash [:profile, :testset], :map => :product, :unique => false
+    reports  = MeegoTestSession.published.select(selection).release(release.name).order(:target, :testset, :product).all
+    profiles = reports.to_nested_hash [:profile, :testset], :map => :product, :unique => false
+    profiles = profiles.map do |profile, testsets|
+      {
+        :name     => profile.capitalize,
+        :testsets => testsets.map do |testset, products| 
+          {
+            :name     => testset,
+            :products => products.map do |product|
+              { :name => product.capitalize }
+            end
+          }
+        end
+      }
+    end
   end
 end
