aae10837ce23d298c0f55966ba7b9d805c34d5bd
Tomi Laine <tomi.laine@leonidasoy.fi>
2011-09-18 10:35:07 +0300

    latest versions of tests

--- features/rename_testset.feature
+++ features/rename_testset.feature
@@ -4,13 +4,14 @@ Feature:
   So that I can better reorganize and manage reports
 
   Background:
+    #Given I have no target labels
     Given I have uploaded reports with profile "Core" having testset "foo"
     And I have uploaded reports with profile "Handset" having testset "foo"
     And I am on the front page
     And I am logged in
     Then show me the page
 
-#  @wip
+  @wip
   @selenium
   Scenario: Rename testset in front page
     When I click on the edit button
@@ -28,7 +29,7 @@ Feature:
     Then I should see testset "bar" for profile "Core"
     And I should see testset "foo" for profile "Handset"
 
-  @wip
+#  @wip
   Scenario: Cancel testset renaming
     When I click on the edit button
     And I edit the testset name "foo" to "bar" for profile "Core"
@@ -45,7 +46,7 @@ Feature:
     Then I should see testset "foo" for profile "Core"
     And I should see testset "foo" for profile "Handset"
 
-  @wip
+#  @wip
   Scenario: Automatic renaming of report titles when testset is renamed
     When I rename the testset "foo" under profile "Core" to "bar"
     And I view the group report for "Core/bar"
--- features/step_definitions/rename_testset_steps.rb
+++ features/step_definitions/rename_testset_steps.rb
@@ -1,5 +1,9 @@
+Given /^I have no target labels$/ do
+  TargetLabel.delete_all
+end
+
 When /^I have uploaded reports with profile "([^"]*)" having testset "([^"]*)"$/ do |profile, testset|
-  FactoryGirl.create(:profile, :label => profile, :normalized => profile.downcase)
+#  FactoryGirl.create(:profile, :label => profile, :normalized => profile.downcase) if TargetLabel.find_by_label(profile).nil?
   FactoryGirl.create_list(:test_report, 2,
     :release => Release.in_sort_order.first,
     :target  => profile.downcase,
@@ -7,11 +11,13 @@ When /^I have uploaded reports with profile "([^"]*)" having testset "([^"]*)"$/
 end
 
 When /^I click on the edit button$/ do
-  When "I press \"#home_edit_link\""
+  When "I press \"home_edit_link\""
 end
 
-When /^I edit the testset name "([^"]*)" to "([^"]*)" for profile "([^"]*)"$/ do |arg1, arg2, arg3|
-  pending # express the regexp above with the code you wish you had
+When /^I edit the testset name "([^"]*)" to "([^"]*)" for profile "([^"]*)"$/ do |orig_name, new_name, profile|
+  testset_id = "#{Release.in_sort_order.first.name}-#{profile}-#{orig_name}"
+  click_on(testset_id)
+  fill_in "input-#{testset_id}", :with => new_name
 end
 
 When /^I press enter$/ do
