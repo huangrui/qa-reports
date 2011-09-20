617e87f625fa3d857d0183152012a490a27b0d3c
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-09-20 17:56:32 +0300

    refactor xml result file parser

--- lib/xml_result_file_parser.rb
+++ lib/xml_result_file_parser.rb
@@ -35,45 +35,37 @@ class XMLResultFileParser
       :result                             => RESULT_MAPPING[test_case['result'].downcase] || MeegoTestCase::NA,
       :comment                            => test_case['comment'] || "",
       :source_link                        => test_case['vcsurl']  || "",
-      :measurements_attributes            => parse_measurements(test_case),
-      :serial_measurements_attributes     => parse_serial_measurements(test_case)
+      :measurements_attributes            => test_case.xpath('./measurement').map do |measurement|
+        {
+          :name       => measurement['name'],
+          :value      => measurement['value'].try(:to_f),
+          :unit       => measurement['unit'],
+          :target     => measurement['target'].try(:to_f),
+          :failure    => measurement['failure'].try(:to_f),
+
+          #TODO: Throw away and order by id
+          :sort_index => 0
+        }
+      end ,
+      :serial_measurements_attributes     => test_case.css('series').map do | series |
+        outline = calculate_outline(series.css('measurement'), series['interval'])
+        {
+          :name          => series['name'],
+          :short_json    => series_json(series.element_children, maxsize=40),
+          :long_json     => series_json_withx(series, outline.interval_unit, maxsize=200),
+          :unit          => series['unit'],
+          :interval_unit => outline.interval_unit,
+
+          :min_value    => outline.minval,
+          :max_value    => outline.maxval,
+          :avg_value    => outline.avgval,
+          :median_value => outline.median,
+
+          #TODO: Throw away and order by id
+          :sort_index   => 0
+        }
+      end
     }
-  end
-
-  def parse_measurements(test_case)
-    test_case.xpath('./measurement').map do |measurement| 
-      {
-        :name       => measurement['name'],
-        :value      => measurement['value'].try(:to_f),
-        :unit       => measurement['unit'],
-        :target     => measurement['target'].try(:to_f),
-        :failure    => measurement['failure'].try(:to_f),
-
-        #TODO: Throw away and order by id
-        :sort_index => 0
-      }
-    end
-  end
-
-  def parse_serial_measurements(test_case)
-    test_case.css('series').map do | series |
-      outline = calculate_outline(series.css('measurement'), series['interval'])
-      {
-        :name          => series['name'],
-        :short_json    => series_json(series.element_children, maxsize=40),
-        :long_json     => series_json_withx(series, outline.interval_unit, maxsize=200),
-        :unit          => series['unit'],
-        :interval_unit => outline.interval_unit,
-
-        :min_value    => outline.minval,
-        :max_value    => outline.maxval,
-        :avg_value    => outline.avgval,
-        :median_value => outline.median,
-
-        #TODO: Throw away and order by id
-        :sort_index   => 0
-      }
-    end
   end
 end
 
