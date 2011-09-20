f1bf303e2ddf7ebc53c87acd7c7d6f8c10e1f4c2
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2011-09-20 15:00:25 +0300

    Use absolute paths instead of relative and hide unpublished reports

--- app/view_models/index.rb
+++ app/view_models/index.rb
@@ -10,21 +10,21 @@ class Index
     TargetLabel.find_by_sql("
       SELECT DISTINCT profiles.label AS profile, reports.testset, reports.product AS name
       FROM target_labels AS profiles
-      LEFT JOIN meego_test_sessions AS reports ON profiles.normalized = reports.target AND reports.release_id = #{release.id}
+      LEFT JOIN meego_test_sessions AS reports ON profiles.normalized = reports.target AND reports.release_id = #{release.id} AND reports.published = TRUE
       ORDER BY profiles.sort_order ASC, testset, product
     ").group_by(&:profile).map do |profile, testsets|
       {
         :name     => profile,
-        :url      => "#{release.name}/#{profile}",
+        :url      => "/#{release.name}/#{profile}",
         :testsets => testsets.first.testset.nil? ? [] : testsets.group_by(&:testset).map do |testset, products|
             {
               :name           => testset,
-              :url            => "#{release.name}/#{profile}/#{testset}",
+              :url            => "/#{release.name}/#{profile}/#{testset}",
               :comparison_url => comparison_url(release, profile, testset),
               :products       => products.map do |product|
                 {
                   :name => product.name,
-                  :url  => "#{release.name}/#{profile}/#{testset}/#{product.name}"
+                  :url  => "/#{release.name}/#{profile}/#{testset}/#{product.name}"
                 }
               end
             }
