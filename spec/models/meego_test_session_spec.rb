8aafef000873ec47386a465ce2ced10ec18a4afe
Tomi Laine <tomi.laine@leonidasoy.fi>
2011-10-05 10:36:28 +0300

    spec test for renaming products in meego_test_session

--- spec/models/meego_test_session_spec.rb
+++ spec/models/meego_test_session_spec.rb
@@ -17,4 +17,20 @@ describe MeegoTestSession do
       report.title.should == "Test report in 2011"
     end
   end
+
+  describe "renaming products" do
+
+    it "should change title accordingly" do
+      report = FactoryGirl.build :test_report, :product => "N900", :title => "Test report for N900 in 2011"
+      report.product = "N950"
+      report.title.should == "Test report for N950 in 2011"
+    end
+
+    it "should not affect title when product name is not there" do
+      report = FactoryGirl.build :test_report, :product => "Pinetrail", :title => "Test report in 2011"
+      report.product = "N950"
+      report.title.should == "Test report in 2011"
+    end
+  end
+
 end
