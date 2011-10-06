d1265ac139e5a344393065df8e05b6524a50d40e
Tomi Laine <tomi.laine@leonidasoy.fi>
2011-10-06 15:54:21 +0300

    rename product, update titles directly

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,10 +13,14 @@ class ProductsController < ApplicationController
   end
 
   def update
-    reports = MeegoTestSession.product_is(params[:product]).readonly(false)
-    reports.find_each { |report| report.product = params[:new_value]; report.save }
+    product, new_value = params[:product], params[:new_value]
+    reports = MeegoTestSession.product_is(product).readonly(false)
+    #reports.find_each { |report| report.product = params[:new_value]; report.save }
     # ensure that also invalid reports (that can't be saved properly) get changed
-    reports.update_all :product => params[:new_value]
+    reports.update_all :title => "replace(title, '#{product}', '#{new_value}'",
+      :product => new_value, :updated_at => DateTime.now
+    #reports.update_all :product => new_value
+    #MeegoTestSession.update_
     head :ok
   end
 end
