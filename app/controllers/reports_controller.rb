85f1c5b40037d46c72ad6bc8652003be08c6e349
Huang Rui <rui.r.huang@intel.com>
2011-10-08 23:52:03 +0800

    Add index_latest into exception of authenticate_user
    Signed-off-by: Huang Rui <rui.r.huang@intel.com>

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -34,7 +34,7 @@ require 'report_exporter'
 class ReportsController < ApplicationController
   include CacheHelper
   layout        'report'
-  before_filter :authenticate_user!,         :except => [:index, :show, :print, :compare]
+  before_filter :authenticate_user!,         :except => [:index, :index_latest, :show, :print, :compare]
   before_filter :validate_path_params,       :only   => [:show, :print]
   cache_sweeper :meego_test_session_sweeper, :only   => [:update, :delete, :publish]
 
