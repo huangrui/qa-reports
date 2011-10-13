e17a290ad03a83a9705850605d8f727cb71ff5b3
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-10-13 14:41:33 +0300

    fixed queries in products_controller and test_sets_controller

--- app/controllers/test_sets_controller.rb
+++ app/controllers/test_sets_controller.rb
@@ -2,10 +2,11 @@ class TestSetsController < ApplicationController
 
   def index
     query_params = {}
-    query_params[:releases] = {:name => params[:release_version]} if params[:release_version]
-    query_params[:target] = params[:target] if params[:target]
+    query_params[:releases] =   {:name  => params[:release_version]} if params[:release_version]
+    query_params[:profiles] =   {:label => params[:target]} if params[:target]
+    query_params[:profiles] ||= {:label => params[:profile].try(:fetch, :label)} if params[:profile]
 
-    @testsets = MeegoTestSession.published.joins(:release).where(query_params).testsets
+    @testsets = MeegoTestSession.published.joins(:release).joins(:profile).where(query_params).testsets
 
     respond_to do |format|
       format.json {render :json => @testsets}
