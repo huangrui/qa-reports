547ff212ef0804bf72019796dd20252f5a66bd57
Huang Rui <rui.r.huang@intel.com>
2011-12-15 15:03:21 +0800

    Update comparison title for build id
    Signed-off-by: Huang Rui <rui.r.huang@intel.com>

--- app/helpers/session_comparison_helper.rb
+++ app/helpers/session_comparison_helper.rb
@@ -1,10 +1,19 @@
 module SessionComparisonHelper
   def comparison_title
-    title = @reports[0].profile.name + ' / ' + @reports[0].testset  + ' / ' +
+    if @reports[0].build_id == @reports[1].build_id
+      title = @reports[0].profile.name + ' / ' + @reports[0].testset  + ' / ' +
          @reports[0].product +  ' / ' + @reports[0].formatted_date + ' vs. '
+    else
+      title = @reports[0].profile.name + ' / ' + @reports[0].testset  + ' / ' +
+         @reports[0].product +  ' / ' + @reports[0].build_id + ' vs. '
+    end
     title += @reports[1].profile.name + ' / ' unless @reports[0].profile.name == @reports[1].profile.name
     title += @reports[1].testset  + ' / ' unless @reports[0].testset == @reports[1].testset
     title += @reports[1].product +  ' / ' unless @reports[0].product == @reports[1].product
-    title += @reports[1].formatted_date
+    if @reports[0].build_id == @reports[1].build_id
+      title += @reports[1].formatted_date
+    else
+      title += @reports[1].build_id
+    end
   end
 end
